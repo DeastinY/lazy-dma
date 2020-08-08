1b50d7cd5fc83cb28cb5ed8110b71c82591bbee1
Richard Polzin <richard.polzin@posteo.de>
2020-08-08 18:29:06 +0200

    finished REST api

--- api.py
+++ api.py
@@ -42,6 +42,35 @@ parser.add_argument('light', type=int, help="Light must be scene id (integer)")
 parser.add_argument('sounds', type=int, help="sounds must be sound id (integer)", action="append")
 
 
+class LightList(Resource):
+    def get(self):
+        return [asdict(l) for l in Light.query.all()]
+
+    def post(self):
+        args = parser.parse_args()
+        light = Light(name=args["name"], description=args["description"])
+        db.session.add(light)
+        db.session.commit()
+        return asdict(light)
+
+
+class LightSingle(Resource):
+    def get(self, light_id):
+        return asdict(Light.query.filter_by(id=light_id).first())
+
+    def patch(self, light_id):
+        args = parser.parse_args()
+        light = Light.query.filter_by(id=light_id).first()
+        light.name = args["name"]
+        light.description = args["description"]
+        db.session.commit()
+        return asdict(light)
+
+    def delete(self, light_id):
+        db.session.delete(Light.query.filter_by(id=light_id).first())
+        db.session.commit()
+
+
 class SceneList(Resource):
     def get(self):
         return [asdict(s) for s in Scene.query.all()]
@@ -56,17 +85,58 @@ class SceneList(Resource):
 
 class SceneSingle(Resource):
     def get(self, scene_id):
-        return Scene.query.filter_by(id=scene_id).first()
+        return asdict(Scene.query.filter_by(id=scene_id).first())
 
     def patch(self, scene_id):
-        return 404
+        args = parser.parse_args()
+        scene = Scene.query.filter_by(id=scene_id).first()
+        scene.name = args["name"]
+        scene.description = args["description"]
+        db.session.commit()
+        return asdict(scene)
 
     def delete(self, scene_id):
-        return 404
+        db.session.delete(Scene.query.filter_by(id=scene_id).first())
+        db.session.commit()
+
+
+class SoundList(Resource):
+    def get(self):
+        return [asdict(s) for s in Sound.query.all()]
+
+    def post(self):
+        args = parser.parse_args()
+        sound = Sound(name=args["name"], description=args["description"])
+        db.session.add(sound)
+        db.session.commit()
+        return asdict(sound)
+
+
+class SoundSingle(Resource):
+    def get(self, sound_id):
+        return asdict(Sound.query.filter_by(id=sound_id).first())
+
+    def patch(self, sound_id):
+        args = parser.parse_args()
+        sound = Sound.query.filter_by(id=sound_id).first()
+        sound.name = args["name"]
+        sound.description = args["description"]
+        db.session.commit()
+        return asdict(sound)
+
+    def delete(self, sound_id):
+        db.session.delete(Sound.query.filter_by(id=sound_id).first())
+        db.session.commit()
 
 
 api.add_resource(SceneList, "/scenes")
 api.add_resource(SceneSingle, "/scenes/<string:scene_id>")
+
+api.add_resource(LightList, "/lights")
+api.add_resource(LightSingle, "/lights/<string:light_id>")
+
+api.add_resource(SoundList, "/sounds")
+api.add_resource(SoundSingle, "/sounds/<string:sound_id>")
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
