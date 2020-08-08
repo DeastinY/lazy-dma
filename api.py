d60f088ff9bba7ac37a5121c715e987f98d8682a
Richard Polzin <richard.polzin@posteo.de>
2020-08-08 17:30:12 +0200

    implemented sqlalchemy

--- api.py
+++ api.py
@@ -1,36 +1,42 @@
-import sqlite3
-from flask import g, Flask
+from dataclasses import dataclass, asdict
+from flask import Flask, jsonify
 from flask_restful import reqparse, Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+CORS(app)
+db = SQLAlchemy(app)
 api = Api(app)
 
-DATABASE = 'lazy-dma.db'
+@dataclass
+class Scene(db.Model):
+    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name: str = db.Column(db.String(60), unique=True, nullable=False)
+    description: str = db.Column(db.String(120), unique=False, nullable=True)
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE, check_same_thread=False)
-    return db
 
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+@dataclass
+class Light(db.Model):
+    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name: str = db.Column(db.String(60), unique=True, nullable=False)
+    description: str = db.Column(db.String(120), unique=False, nullable=True)
 
-with app.app_context():
-    conn = get_db()
-    c = conn.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS SCENES (ID INTEGER PRIMARY KEY, NAME TEXT, TAGS TEXT)")
-    c.execute("CREATE TABLE IF NOT EXISTS SOUNDS (ID INTEGER PRIMARY KEY, NAME TEXT, TAGS TEXT)")
-    c.execute("CREATE TABLE IF NOT EXISTS LIGHTS (ID INTEGER PRIMARY KEY, NAME TEXT, TAGS TEXT)")
-    c.execute("CREATE TABLE IF NOT EXISTS SCENES_SOUNDS (SCENE_ID INTEGER, SOUND_ID INTEGER)")
-    c.execute("CREATE TABLE IF NOT EXISTS SCENES_LIGHTS (SCENE_ID INTEGER UNIQUE, LIGHT_ID INTEGER)")
-    conn.commit()
+
+@dataclass
+class Sound(db.Model):
+    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name: str = db.Column(db.String(60), unique=True, nullable=False)
+    description: str = db.Column(db.String(120), unique=False, nullable=True)
+
+
+db.create_all()
+db.session.commit()
 
 parser = reqparse.RequestParser()
 parser.add_argument('name', type=str)
+parser.add_argument('description', type=str)
 parser.add_argument('tags', type=str)
 parser.add_argument('light', type=int, help="Light must be scene id (integer)")
 parser.add_argument('sounds', type=int, help="sounds must be sound id (integer)", action="append")
@@ -38,59 +44,29 @@ parser.add_argument('sounds', type=int, help="sounds must be sound id (integer)"
 
 class SceneList(Resource):
     def get(self):
-        c = get_db().cursor()
-        c.execute("SELECT * FROM SCENES;")
-        return c.fetchall()
+        return [asdict(s) for s in Scene.query.all()]
 
     def post(self):
-        conn = get_db()
-        c = conn.cursor()
         args = parser.parse_args()
-        c.execute("INSERT INTO SCENES (NAME, TAGS) VALUES (?, ?)", [args["name"], args["tags"]])
-        for sid in args["sounds"]:
-            c.execute("INSERT INTO SCENES_SOUNDS (SCENE_ID, SOUND_ID) VALUES (?, ?)", [c.lastrowid, sid])
-        c.execute("INSERT INTO SCENES_LIGHTS (SCENE_ID, LIGHT_ID) VALUES (?, ?)", [c.lastrowid, args["light"]])
-        conn.commit()
-        return c.lastrowid, 201
+        scene = Scene(name=args["name"], description=args["description"])
+        db.session.add(scene)
+        db.session.commit()
+        return asdict(scene)
 
 
-class Scene(Resource):
+class SceneSingle(Resource):
     def get(self, scene_id):
-        c = get_db().cursor()
-        c.execute("SELECT * FROM SCENES WHERE ID=?", [scene_id])
-        return c.fetchone()
+        return Scene.query.filter_by(id=scene_id).first()
 
     def patch(self, scene_id):
-        conn = get_db()
-        c = conn.cursor()
-        args = parser.parse_args()
-        c.execute("UPDATE SCENES SET NAME=?, TAGS=? WHERE ID=?", [args["name"], args["tags"], scene_id])
-        c.execute("DELETE FROM SCENES_SOUNDS WHERE SCENE_ID=?", [scene_id])
-        for sid in args["sounds"]:
-            c.execute("INSERT INTO SCENES_SOUNDS (SCENE_ID, SOUND_ID) VALUES (?, ?)", [c.lastrowid, sid])
-        c.execute("DELETE FROM SCENES_LIGHTS WHERE SCENE_ID=?", [scene_id])
-        c.execute("INSERT INTO SCENES_LIGHTS (SCENE_ID, LIGHT_ID) VALUES (?, ?)", [c.lastrowid, args["light"]])
-        conn.commit()
-        return 201
+        return 404
 
     def delete(self, scene_id):
-        conn = get_db()
-        c = conn.cursor()
-        c.execute("DELETE FROM SCENES_SOUNDS WHERE SCENE_ID=?", [scene_id])
-        c.execute("DELETE FROM SCENES_LIGHTS WHERE SCENE_ID=?", [scene_id])
-        c.execute("DELETE FROM SCENES WHERE ID=?", [scene_id])
-        conn.commit()
-        return "", 204
+        return 404
 
 
 api.add_resource(SceneList, "/scenes")
-api.add_resource(Scene, "/scenes/<string:scene_id>")
-
-
-@app.teardown_appcontext
-def close_connection(exception):
-    conn.close()
-
+api.add_resource(SceneSingle, "/scenes/<string:scene_id>")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
